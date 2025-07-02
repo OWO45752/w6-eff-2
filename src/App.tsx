01241e50c09d8f9429716391e16e9e1f4e36b18d
OwO <anon.mong@sbac.ac.th>
2025-07-02 09:57:03 +0700

    asf

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { Card, Col, Image, Row } from "antd";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<Record<string, never>[]>([]);
+
+  useEffect(() => {
+    (async () => {
+        const r = await fetch("https://dummyjson.com/products")
+        const d = await r.json();
+        setData(d.products);
+    })();
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Row gutter={[20, 20]}>
+            {data.map((e) => (
+            <Col>
+            <Card key={e.id} title={e.title} style={{ width: 240 }} hoverable>
+                <Image src={e.images[0]} />
+                <p>{e.description}</p>
+                <p>price: {e.price}</p>
+            </Card></Col>
+        ))}
+        </Row>
     </>
   )
 }
